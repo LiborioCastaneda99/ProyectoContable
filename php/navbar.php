8ad1ce5177170cac64eb54d389319f2f0c52b57c
“Liborio” <libodjyj@gmail.com>
2020-11-12 18:33:58 -0500

    Proyecto fase 1

---
+++ php/navbar.php
@@ -0,0 +1,47 @@
+        <nav id="sidebar">
+            <div id="dismiss">
+                <i class="fas fa-arrow-left"></i>
+            </div>
+
+            <div class="sidebar-header text-center">
+                <h3>TItulo</h3>
+            </div>
+
+            <ul class="list-unstyled components">
+                <li class="">
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">Opcion 1</a>
+                    <ul class="collapse list-unstyled" id="homeSubmenu">
+                        <li>
+                            <a href="#">Opcion 1</a>
+                        </li>
+                        <li>
+                            <a href="#">Opcion 2</a>
+                        </li>
+                        <li>
+                            <a href="#">Opcion 3</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Opcion 2</a>
+                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false">Opcion 3</a>
+                    <ul class="collapse list-unstyled" id="pageSubmenu">
+                        <li>
+                            <a href="#">Opcion 1</a>
+                        </li>
+                        <li>
+                            <a href="#">Opcion 2</a>
+                        </li>
+                        <li>
+                            <a href="#">Opcion 3</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Opcion 4</a>
+                </li>
+                <li>
+                    <a href="#">Opcion 5</a>
+                </li>
+            </ul>
+        </nav>
